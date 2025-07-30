a1ae2dfe254e25d953a4e72959e5feb12eb551be
NITHI2209 <nithyasree065@gmail.com>
2025-07-30 20:06:43 +0530

    Create Add Digits.py

---
+++ Add Digits.py
@@ -0,0 +1,18 @@
+#question:
+# Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+# Example 1:
+# Input: num = 38
+# Output: 2
+# Explanation: The process is
+# 38 --> 3 + 8 --> 11
+# 11 --> 1 + 1 --> 2 
+# Since 2 has only one digit, return it.
+#code:
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num >=10:
+            total = 0
+            for i in str(num):
+                total += int(i)
+            num = total
+        return num
