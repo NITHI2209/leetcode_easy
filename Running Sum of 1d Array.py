a48ea129ff6cec82affe38bd6547510f020b1565
NITHI2209 <nithyasree065@gmail.com>
2025-07-30 21:40:49 +0530

    Create  Running Sum of 1d Array.py

---
+++ Running Sum of 1d Array.py
@@ -0,0 +1,16 @@
+#question
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+# Return the running sum of nums.
+# Example 1:
+# Input: nums = [1,2,3,4]
+# Output: [1,3,6,10]
+# Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+#code:
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        total = 0
+        result = []
+        for i in nums:
+            total += i
+            result.append(total)
+        return result
