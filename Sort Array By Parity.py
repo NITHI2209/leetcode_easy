911f153a835a50d4d5b06c233fecc4b3252c31e1
NITHI2209 <nithyasree065@gmail.com>
2025-07-31 12:01:34 +0530

    Create Sort Array By Parity.py

---
+++ Sort Array By Parity.py
@@ -0,0 +1,20 @@
+#question
+# Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+# Return any array that satisfies this condition.
+# Example 1:
+# Input: nums = [3,1,2,4]
+# Output: [2,4,3,1]
+# Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+#code:
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        output = []
+        output1 = []
+        for i in nums:
+            if i % 2 == 0:
+                output.append(i)
+        for j in nums:
+            if j % 2 != 0:
+                output1.append(j)
+        result = output + output1
+        return result
