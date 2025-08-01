4055c463758ad3562783fb147be283b12cc2574d
NITHI2209 <nithyasree065@gmail.com>
2025-08-01 18:17:17 +0530

    Create reverse_string_II.py

---
+++ reverse_string_II.py
@@ -0,0 +1,17 @@
+#question
+# Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
+# If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
+# Example 1:
+# Input: s = "abcdefg", k = 2
+# Output: "bacdfeg"
+# Example 2:
+# Input: s = "abcd", k = 2
+# Output: "bacd"
+#code:
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        lst = list(s)
+        for i in range(0,len(lst),2*k):
+            lst[i:i+k] = reversed(lst[i:i+k])
+        result = "".join(lst)
+        return result
