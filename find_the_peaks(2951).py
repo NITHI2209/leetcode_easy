449882b3fb287667f6d3d5587bc4088f226f055d
NITHI2209 <nithyasree065@gmail.com>
2025-08-11 22:59:12 +0530

    find the peaks

---
+++ find_the_peaks(2951).py
@@ -0,0 +1,20 @@
+#question
+# You are given a 0-indexed array mountain. Your task is to find all the peaks in the mountain array.
+# Return an array that consists of indices of peaks in the given array in any order.
+# Notes:
+# A peak is defined as an element that is strictly greater than its neighboring elements.
+# The first and last elements of the array are not a peak.
+# Example 1:
+# Input: mountain = [2,4,4]
+# Output: []
+# Explanation: mountain[0] and mountain[2] can not be a peak because they are first and last elements of the array.
+# mountain[1] also can not be a peak because it is not strictly greater than mountain[2].
+# So the answer is [].
+#code:
+class Solution:
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+        peaks = []
+        for i in range(1,len(mountain)-1):
+            if mountain[i] > mountain[i-1] and mountain[i] > mountain[i+1]:
+                peaks.append(i)
+        return peaks
