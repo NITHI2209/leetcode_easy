f7fc1c8ac6a80bf0c529c91b9b5b5a9c053a1c88
NITHI2209 <nithyasree065@gmail.com>
2025-08-17 20:26:59 +0530

    longest common perfix

---
+++ longest_common_perfix(14).py
@@ -0,0 +1,20 @@
+#question
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+# Example 1:
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+# Example 2:
+# Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+#code
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        perfix = ""
+        for i in zip(*strs):
+            if len(set(i)) == 1:
+                perfix += i[0]
+            else:
+                break
+        return perfix
