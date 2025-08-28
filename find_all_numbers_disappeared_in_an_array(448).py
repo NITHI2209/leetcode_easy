96dd9f53cd0c4204a099d1775088d471c60aa201
NITHI2209 <nithyasree065@gmail.com>
2025-08-28 20:53:24 +0530

    find all numbers disappeared in an array

---
+++ find_all_numbers_disappeared_in_an_array(448).py
@@ -0,0 +1,13 @@
+#question
+# Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+# Example 1:
+# Input: nums = [4,3,2,7,8,2,3,1]
+# Output: [5,6]
+# Example 2:
+# Input: nums = [1,1]
+# Output: [2]
+#code
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        return list(set(range(1,n+1))-set(nums))
