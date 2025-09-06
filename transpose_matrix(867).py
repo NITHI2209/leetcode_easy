aa73e88f14da5b4f1cb3a329690e213e78a0b826
NITHI2209 <nithyasree065@gmail.com>
2025-09-06 14:01:04 +0530

    transpose matrix

---
+++ transpose_matrix(867).py
@@ -0,0 +1,19 @@
+#question
+# Given a 2D integer array matrix, return the transpose of matrix.
+# The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+# Example 1:
+# Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# Output: [[1,4,7],[2,5,8],[3,6,9]]
+# Example 2:
+# Input: matrix = [[1,2,3],[4,5,6]]
+# Output: [[1,4],[2,5],[3,6]]
+#code
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        result = [[0] * rows for _ in range(cols)]
+        for i in range(rows):
+            for j in range(cols):
+                result[j][i] = matrix[i][j]
+        return result
