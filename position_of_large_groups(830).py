d0cbd681920c65c82f7a96cee659bc11fcd135ec
NITHI2209 <nithyasree065@gmail.com>
2026-07-22 22:57:34 +0530

    created

---
+++ position_of_large_groups(830).py
@@ -0,0 +1,15 @@
+# 
+
+class Solution:
+    def largeGroupPositions(self, s: str) -> List[List[int]]:
+        start = 0
+        result = []
+        for i in range(1, len(s)):
+            if s[i] != s[i - 1]:
+                if i - start >= 3:
+                    result.append([start, i - 1])
+                start = i
+        if len(s) - start >= 3:
+            result.append([start, len(s) - 1])
+
+        return result
