5b81ea095b7598dfda09c57442da88e77078f4c8
NITHI2209 <nithyasree065@gmail.com>
2026-07-23 19:01:28 +0530

    created

---
+++ Teemo_attacking(495).py
@@ -0,0 +1,13 @@
+# 
+
+class Solution:
+    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        total = 0
+        for i in range(len(timeSeries)-1):
+            gap = timeSeries[i+1] - timeSeries[i]
+            if gap < duration:
+                total += gap
+            else:
+                total += duration
+        return total + duration
+        
