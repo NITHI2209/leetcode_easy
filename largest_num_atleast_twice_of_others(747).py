3248faf9c521fd5fbb768c58cdefe9bc02e92216
NITHI2209 <nithyasree065@gmail.com>
2026-07-24 19:36:11 +0530

    created

---
+++ largest_num_atleast_twice_of_others(747).py
@@ -0,0 +1,31 @@
+# You are given an integer array nums where the largest integer is unique.
+
+# Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+
+ 
+
+# Example 1:
+
+# Input: nums = [3,6,1,0]
+# Output: 1
+# Explanation: 6 is the largest integer.
+# For every other number in the array x, 6 is at least twice as big as x.
+# The index of value 6 is 1, so we return 1.
+# Example 2:
+
+# Input: nums = [1,2,3,4]
+# Output: -1
+# Explanation: 4 is less than twice the value of 3, so we return -1.
+
+class Solution:
+    def dominantIndex(self, nums: List[int]) -> int:
+        largest = max(nums)
+        index = nums.index(largest)
+
+        for i in nums:
+            if largest == i:
+                continue
+            if largest < 2 * i:
+                return -1
+        return index
+ 
