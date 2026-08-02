3a19afeac0ab75e5f2b6164ce2f8b5d791385bd1
NITHYA SREE S <nithyasree065@gmail.com>
2026-08-02 21:28:22 +0530

    Update Detect_Capital(520).py

--- Detect_Capital(520).py
+++ Detect_Capital(520).py
@@ -4,7 +4,7 @@ All letters in this word are not capitals, like "leetcode".
 Only the first letter in this word is capital, like "Google".
 Given a string word, return true if the usage of capitals in it is right.
 Example 1:
-Input: word = "USA"
+Input: word = "USA"  
 Output: true
 Example 2:
 Input: word = "FlaG"
